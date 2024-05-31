3b642f0f010b52b22c6533596f65d017843f69d0
GiampyLux <giampaolopaladino01@gmail.com>
2024-05-31 15:30:50 +0200

    PD2

--- todolist/src/app/pages/home/home.component.ts
+++ todolist/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Obiettivi } from '../../models/obiettivi';
+import { Utenti } from '../../models/utenti';
+import { ObiettiviService } from '../../services/obiettivi.service';
+import { PersoneService } from '../../services/persone.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+obbArray: Obiettivi[] = []
+persArray: Utenti[] = [];
 
+constructor(private obbSvc:ObiettiviService, private persSvc:PersoneService) {}
+
+ngOnInit() {
+  this.obbArray = this.obbSvc.obbArr;
+  this.persArray = this.persSvc.persArr;
+}
+getPersonId(userId:number):Utenti | undefined {
+  return this.persArray.find(user => user.id === userId);
+}
 }
